30e348e5d996d23da60fd1a4ee0af51c76f6d2ca
Donte Trumble <dt.trumble@gmail.com>
2018-07-25 00:05:53 -0400

    setting up form for payments

--- src/components/Checkout/Checkout.vue
+++ src/components/Checkout/Checkout.vue
@@ -10,7 +10,7 @@
                 <v-form v-model="step1Form">
                     <v-container>
                         <v-layout row wrap>
-                            <v-flex xs12 sm6 md4>
+                            <v-flex xs12 sm6 md3>
                                 <v-text-field
                                 v-model="firstName"
                                 label="First Name"
@@ -19,7 +19,7 @@
                                 
                                 </v-text-field>
                             </v-flex>
-                            <v-flex xs12 sm6 md4>
+                            <v-flex xs12 sm6 md3>
                                 <v-text-field
                                 v-model="lastName"
                                 label="Last Name"
@@ -28,11 +28,20 @@
                                 
                                 </v-text-field>
                             </v-flex>
-                            <v-flex xs12 sm6 md4>
+                            <v-flex xs12 sm6 md3>
                                 <v-text-field
                                 v-model="email"
                                 label="Email"
                                 :rules="emailRules"
+                                required>
+                                
+                                </v-text-field>
+                            </v-flex>
+                            <v-flex xs12 sm6 md3>
+                                <v-text-field
+                                v-model="phoneNumber"
+                                label="Phone Number"
+                                :rules="phoneNumberRules"
                                 required>
                                 
                                 </v-text-field>
@@ -44,11 +53,69 @@
             <v-btn color="primary" @click="e6 = 2" :disabled="!step1Form">Continue</v-btn>
             </v-stepper-content>
 
-            <v-stepper-step :complete="e6 > 2" step="2">Shipping Address</v-stepper-step>
+            <v-stepper-step :complete="e6 > 2" step="2">Shipping/Billing Address</v-stepper-step>
 
             <v-stepper-content step="2">
-            <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
-            <v-btn color="primary" @click="e6 = 3">Continue</v-btn>
+            <v-card color="grey lighten-3" class="mb-5 pa-4 elevation-5">
+                
+                <v-form v-model="step2Form">
+                    <v-container>
+                        <v-checkbox
+                            label="Same as billing address?"
+                            v-model="checkIfBillingIsShipping"
+                            ></v-checkbox>
+                        <v-layout row wrap>
+                            <v-flex xs12 sm6 md10>
+                                <v-text-field
+                                v-model="shipping_address.line_2"
+                                :rules="GenericRules"
+                                label="Address"
+                                required>
+                                
+                                </v-text-field>
+                            </v-flex>
+                            <v-flex xs12 sm6 md2>
+                                <v-text-field
+                                    v-model="shipping_address.line_1"
+                                    label="APT#">
+                                
+                                </v-text-field>
+                            </v-flex>
+                            <v-flex xs12 sm6 md3>
+                                <v-text-field
+                                    v-model="shipping_address.county"
+                                    label="State"
+                                    :rules="GenericRules"
+                                    required>
+                                
+                                </v-text-field>
+                                
+                            </v-flex>
+                            <v-flex xs12 sm6 md3>
+                                <v-text-field
+                                    v-model="billing_address.city"
+                                    label="City"
+                                    :rules="GenericRules"
+                                    required>
+                                
+                                </v-text-field>
+                                
+                            </v-flex>
+                            <v-flex xs12 sm6 md3>
+                                <v-text-field
+                                    v-model="shipping_address.postcode"
+                                    label="Postal Code"
+                                    :rules="PostalRules"
+                                    required>
+                                
+                                </v-text-field>
+                                
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </v-form>
+            </v-card>
+            <v-btn color="primary" @click="e6 = 3" :disabled="!step2Form">Continue</v-btn>
             <v-btn flat @click="e6 = 1">GO BACK</v-btn>
             </v-stepper-content>
 
@@ -66,7 +133,9 @@
             <v-btn color="primary" @click="e6 = 1">Continue</v-btn>
             <v-btn flat>Cancel</v-btn>
             </v-stepper-content>
+            {{this.email}}
         </v-stepper>
+        
 </template>
 
 <script>
@@ -78,19 +147,67 @@
                 firstName: null,
                 lastName: null,
                 email: null,
+                phoneNumber: null,
+                checkIfBillingIsShipping: true,
                 step1Form: false,
+                step2Form: false,
                 nameRules: [
                     v => !!v || 'Name is required',
                     v => (v && v.length >= 2) || 'Name must be greater than 2 characters'
                 ],
+                phoneNumberRules: [
+                    v => !!v || 'Phone Number is required',
+                    v => (v && v.length >= 10) || 'Phone Number must include area code.'
+                ],
                 emailRules: [
                     v => !!v || 'E-mail is required',
                     v => /.+@.+/.test(v) || 'E-mail must be valid'
-                ]
+                ],
+                GenericRules: [
+                    v => !!v || 'Address is required',
+                    v => (v && v.length >= 2)  || 'Address must be valid'
+                ],
+                PostalRules:[
+                    v => !!v || 'Postal Code is required',
+                    v => (v && v.length === 5)  || 'Postal Code must be valid'
+                ],
+                customer: {
+                    name: this.fullName,
+                    email: this.email
+                },
+                shipping_address: {
+                    first_name: "",
+                    last_name: "",
+                    phone_number: "",
+                    company_name: "",
+                    line_1: "",
+                    line_2: "",
+                    city: "",
+                    postcode: "",
+                    county: "",
+                    country: "USA"
+                },
+                billing_address: {
+                    first_name: "",
+                    last_name: "",
+                    company_name: "",
+                    line_1: "",
+                    line_2: "",
+                    city: "",
+                    postcode: "",
+                    county: "",
+                    country: ""
+                }
             }
         },
         created(){
             this.$store.dispatch('getCartItems')
+            this.$store.dispatch('getCartStoreLength')
+        },
+        computed:{
+            fullName(){
+                return `${this.firstName} ${this.lastName}`;
+            }
         }
     }
 </script>
